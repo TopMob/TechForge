f1bc7165bd372c0b0bc6ce6228222005702f4658
TopMob <103371844+TopMob@users.noreply.github.com>
2026-02-26 19:18:08 +0300

    Remove SmartNotes legacy modules and keep only TechForge runtime

--- js/editor/core-utils.js
+++
@@ -1,4 +0,0 @@
-export const getCountLabel = (count, singleKey, pluralKey) => {
-    if (count === 1) return UI.getText(singleKey, singleKey)
-    return UI.getText(pluralKey, UI.getText(singleKey, singleKey))
-}
--- js/notes/domain/notesAggregationService.js
+++
@@ -1,9 +0,0 @@
-const NotesAggregationService = (() => {
-    function aggregate(ownedNotes, sharedNotes, activeUid) {
-        const mergedNotes = NotesStorage.applyStoredFavorites([...(ownedNotes || []), ...(sharedNotes || [])])
-        if (activeUid) NotesStorage.writeCachedNotes(activeUid, mergedNotes)
-        NotesStorage.syncFavoritesStorage(mergedNotes)
-        return mergedNotes
-    }
-    return { aggregate }
-})()
